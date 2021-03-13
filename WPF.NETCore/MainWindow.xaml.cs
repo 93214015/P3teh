f6ed8d756b62626c5daf63bf675a8e8d8895eca3
mamin <8703303@gmail.com>
2021-03-13 19:03:04 +0330

    Updated Main Form

--- WPF.NETCore/MainWindow.xaml.cs
+++ WPF.NETCore/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,29 @@ namespace WPF.NETCore
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Storyboard _StbStartApp = (Storyboard)this.FindResource("STBStartApp");
+            _StbStartApp.Begin(this);
+        }
+
+        private void BtnMenuLeft_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Storyboard STBLeftMenu = (Storyboard)this.FindResource("STBLeftMenu");
+            STBLeftMenu.Begin();
+        }
+
+        private void BtnLeftMenuClose_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Storyboard STBLeftMenuReverse = (Storyboard)this.FindResource("STBLeftMenuReverse");
+            STBLeftMenuReverse.Begin();
         }
     }
 }
